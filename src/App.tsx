b53731c9fc579f865bbfd2069a5676965deb94b4
Mustafa Nawzat <mustafanawzatt@gmail.com>
2022-12-20 08:32:54 +0300

    chore: setup Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import { Routes, Route, Navigate } from "react-router-dom"
 function App() {
 
   return (
-    <div className="App">
-      <h1>Hi, This is a note taking app.</h1>
-    </div>
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<div>New</div>} />
+        <Route path="/:id">
+          <Route index element={<div>Show</div>} />
+          <Route path="edit" element={<div>Edit</div>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+
+      </Routes>
+    </Container>
   )
 }
 
